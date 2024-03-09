3a5460bbd7fa8471ae1c2bd0eb819a0cffb96b04
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2024-03-09 12:40:32 +0530

    Change password feature added

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,67 @@
-import classes from './ProfileForm.module.css';
+import React, { useRef, useContext } from "react";
+
+import AuthContext from "../../context/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newPassword = newPasswordInputRef.current.value;
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBCJtOzrzr4uqoOFqITAFOUwxojDJqyWoQ`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          password: newPassword,
+          idToken: authCtx.token,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
