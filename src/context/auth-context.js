f0c8699d03b5523565245acb135a612c3a3f79cb
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2024-03-09 14:37:11 +0530

    Auto logout featute implemented

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -7,11 +7,34 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = () => {
+  const currentTime = new Date().getTime();
+  const expirationTime = localStorage.getItem("expirationTime");
+  const adjExpirationTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjExpirationTime - currentTime;
+  return remainingDuration;
+};
+
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const remainingTime = calculateRemainingTime();
+  console.log(remainingTime);
+  if (remainingTime <= 0) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+  return storedToken;
+};
+
 export const AuthContextProvider = (props) => {
-  const tokenFromLs = localStorage.getItem("token");
-  const [token, setToken] = useState(tokenFromLs);
+  const tokenData = retrieveStoredToken();
+  const [token, setToken] = useState(tokenData);
   const userLoggedIn = !!token;
+
   const loginHandler = (token) => {
+    const expirationTime = new Date(new Date().getTime() + 60 * 1000);
+    localStorage.setItem("expirationTime", expirationTime.toISOString());
     localStorage.setItem("token", token);
     setToken(token);
   };
